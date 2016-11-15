180b1db4d6602ffcbaa0507674e6ebe1bea76721
wowshakhov <m8r-uiq5041@mailinator.com>
2016-11-15 15:52:48 +0700

    review fixes

--- apa.js
+++ apa.js
@@ -1,18 +1,17 @@
-/*
-Calculates arbitrary precision sum of two numbers
-str1 (string) - term 1, integer number with an optional preceding plus or minus sign
-str2 (string) - term 2
+const base = 10;
 
-returns sum str1 + str2 (string)
-*/
+/**
+ * Calculates arbitrary precision sum of two numbers
+ * @param  {string} str1 Integer number with an optional preceding plus or minus sign
+ * @param  {string} str2 Integer number with an optional preceding plus or minus sign
+ * @return {string} Sum: str1 + str2 
+ */
 function apsum(str1, str2) {
-    //check if operands are numeric
-    if (!str1.match(/^(\-)?\d+$/) || !str2.match(/^(\-)?\d+$/)) { 
-        throw new Error('NaN');
-    }
+    // check if operands are numeric
+    checkArgs(str1, str2);
     let x = [];
     let y = [];
-    //convert string to array
+    // convert string to array
     if (str1.length > 1) { 
         x = str1.split('').reverse();
     } else {
@@ -24,36 +23,26 @@ function apsum(str1, str2) {
         y[0] = str2[0];
     }
     let s = [];
-    //handle minus signs
-    if (str1[0] == '-' && str2[0] != '-') {
-        str1 = str1.substring(1);
-        return apsub(str2, str1);
+    // handle minus signs
+    if (str1[0] === '-' && str2[0] !== '-') {
+        return apsub(str2, str1.substring(1));
     }
-    if (str1[0] != '-' && str2[0] == '-') {
-        str2 = str2.substring(1);
-        return apsub(str1, str2);
+    if (str1[0] !=='-' && str2[0] === '-') {
+        return apsub(str1, str2.substring(1));
     }
-    if (str1[0] == '-' && str2[0] == '-') {
-        str1 = str1.substring(1);
-        str2 = str2.substring(1);
-        return '-' + apsum(str1, str2);
+    if (str1[0] === '-' && str2[0] === '-') {
+        return '-' + apsum(str1.substring(1), str2.substring(1));
     }
     let length = Math.max(x.length, y.length);
     let ds = 0;
-    //convert array elements to numbers
-    x = x.map((n) => {
-        return Number(n);
-    });
-    y = y.map((n) => {
-        return Number(n);
-    });
+    // convert array elements to numbers
     for (let i = 0; i < length; ++i) {
         if (!x[i]) x[i] = 0;
         if (!y[i]) y[i] = 0;
-        //calculate one digit of sum and carry
-        ds = x[i] + y[i] + ds;
-        if (ds >= 10) {
-            ds -= 10;
+        // calculate one digit of sum and carry
+        ds = Number(x[i]) + Number(y[i]) + ds;
+        if (ds >= base) {
+            ds -= base;
             s[i] = ds;
             ds = 1;
         } else {
@@ -61,60 +50,67 @@ function apsum(str1, str2) {
             ds = 0;
         }
     }
-    //add carry as a digit
+    // add carry as a digit
     if (ds) {
         s.push(ds);
     }
-    //convert to string
+    // convert to string
     s = s.reverse();
     s = s.join('');
-    if (s == '0') {
+    if (s === '0') {
         return '0';
     }
-    //remove leading zeroes
+    return removeLeadingZeroes(s);
+}
+
+/**
+ * Removes leading zeroes from a string
+ * @param  {string} str1
+ * @return {string} str1 without leading zeroes
+ */
+
+function removeLeadingZeroes(s) {
     let count = 0;
     let it = 0;
-    while (s[it++] == '0') {
+    while (s[it++] === '0') {
         count++;
     }
     s = s.substring(count);
-    if (s.length == 0) {
+    if (s.length === 0) {
         return '0';
     }
     return s;
 }
 
-/*
-Calculate arbitrary precision difference
-str1 (string) - term 1, integer number  with optional preceding plus or minus sign
-str2 (string) - term 2
-
-returns diffrence str1 - str2 (string)
-*/
-function apsub(str1, str2) {
-    if (!str1.match(/^(\-)?\d+$/) || !str2.match(/^(\-)?\d+$/)) {
+function checkArgs(str1, str2) {
+    if (!str1.match(/^(\-)?\d+$/) || !str2.match(/^(\-)?\d+$/)) { 
         throw new Error('NaN');
     }
+}
+
+/**
+ * Calculates arbitrary precision difference of two numbers
+ * @param  {string} str1 Integer number with an optional preceding plus or minus sign
+ * @param  {string} str2 Integer number with an optional preceding plus or minus sign
+ * @return {string} Difference str1 - str2
+ */
+function apsub(str1, str2) {
+    checkArgs(str1, str2);
     let xl = str1.length;
     let yl = str2.length;
     let less = 0;
-    if (str1[0] == '-' && str2[0] != '-') {
-        str1 = str1.substring(1);
-        return '-' + apsum(str1, str2);
+    if (str1[0] === '-' && str2[0] !=='-') {
+        return '-' + apsum(str1.substring(1), str2);
     }
-    if (str1[0] != '-' && str2[0] == '-') {
-        str2 = str2.substring(1);
-        return apsum(str1, str2);
+    if (str1[0] !=='-' && str2[0] === '-') {
+        return apsum(str1, str2.substring(1));
     }
-    if (str1[0] == '-' && str2[0] == '-') {
-        str1 = str1.substring(1);
-        str2 = str2.substring(1);
-        return apsub(str2, str1);
+    if (str1[0] === '-' && str2[0] === '-') {
+        return apsub(str2.substring(1), str1.substring(1));
     }
-    //swap terms if necessary
-    if ((xl < yl) || ((xl == yl) && (str1 < str2))) {
-        let t;
-        t = str1;
+    // swap terms if necessary
+    if ((xl < yl) || ((xl === yl) && (str1 < str2))) {
+        let t = str1;
         str1 = str2;
         str2 = t;
         less = 1;
@@ -124,18 +120,13 @@ function apsub(str1, str2) {
     let s = [];
     let dd = 0;
     let length = Math.max(xl, yl);
-    x = x.map((n) => {
-        return Number(n);
-    });
-    y = y.map((n) => {
-        return Number(n);
-    });
+
     for (let i = 0; i < length; ++i) {
         if (!x[i]) x[i] = 0;
         if (!y[i]) y[i] = 0;
-        dd = x[i] - y[i] + dd;
+        dd = Number(x[i]) - Number(y[i]) + dd;
         if (dd < 0) {
-            dd += 10;
+            dd += base;
             s[i] = dd;
             dd = -1;
         } else {
@@ -145,46 +136,41 @@ function apsub(str1, str2) {
     }
     s = s.reverse();
     s = s.join('');
-    let count = 0;
-    let it = 0;
-    while (s[it++] == '0') {
-        count++;
+    s =  removeLeadingZeroes(s);
+    if (less) {
+        s *= -1;
     }
-    s = s.substring(count);
-    if (less) s *= -1;
-    if (s.length == 0) return '0';
     return s;
+
 }
 
-/*
-Auxiliary function for apmul
-str1 (string) - term 1, integer number with optional preceding plus or minus sign
-str2 (number) - term 2
-shift (number) - adds trailing zeroes to the result
-
-returns product str1 * multiplier with trailing zeroes (string)
-*/
+/**
+ * Auxiliary function for apmul
+ * @param  {string} str1 Integer number with an optional preceding plus or minus sign
+ * @param  {string} multiplier Integer number with an optional preceding plus or minus sign\
+ * @param  {string} shift Number of trailing zeroes in the result
+ * @return {string} Product str1 * multiplier with trailing zeroes
+ */
 function _apmul(str1, multiplier, shift) {
     let x = str1.split('').reverse();
     let s = [];
     let ds = 0;
     let length = x.length;
-    x = x.map(function(n) {
-        return Number(n);
-    });
     for (let i = 0; i < length; ++i) {
         if (!x[i]) x[i] = 0;
-        ds = x[i] * multiplier + ds;
-        //calculate carry for multiplication
-        if (ds >= 10) {
-            s[i] = ds % 10;
-            ds = Math.floor(ds / 10);
+        ds = Number(x[i]) * multiplier + ds;
+        // calculate carry for multiplication
+        if (ds >= base) {
+            s[i] = ds % base;
+            ds = Math.floor(ds / base);
         } else {
             s[i] = ds;
             ds = 0;
         }
     }
-    if (ds) s.push(ds);
+    if (ds) {
+        s.push(ds);
+    }
     for (let i = 0; i < shift; ++i) {
         s.unshift(0);
     }
@@ -193,48 +179,40 @@ function _apmul(str1, multiplier, shift) {
     return s;
 }
 
-/*
-Calculates arbitrary precision product of two numbers
-str1 (string) - term 1, integer number with an optional preceding plus or minus sign
-str2 (string) - term 2
-
-returns product str1 * str2 (string)
-*/
+/**
+ * Calculate arbitrary precision product of two numbers
+ * @param  {string} str1 Integer number with an optional preceding plus or minus sign
+ * @param  {string} str2 Integer number with an optional preceding plus or minus sign
+ * @return {string} Product str1 * str2
+ */
 function apmul(x, y) {
-    if (!x.match(/^(\-)?\d+$/) || !y.match(/^(\-)?\d+$/)) {
-        throw new Error('NaN');
-    }
+    checkArgs(x, y);
     let s = 0;
     let less = 0;
-    //handle minus signs
-    if (x[0] == '-') {
+    // handle minus signs
+    if (x[0] === '-') {
         x = x.substring(1);
         less ^= 1;
     }
-    if (y[0] == '-') {
+    if (y[0] === '-') {
         y = y.substring(1);
         less ^= 1;
     }
     let length = y.length;
-    //multiply the 1st term by one digit of the 2nd term and add to the result with trailing zeroes
+    // multiply the 1st term by one digit of the 2nd term and add to the result with trailing zeroes
     for (let i = 0; i < length; ++i) {
         s = apsum(s.toString(), _apmul(x, y[length - i - 1], i));
     }
-    let count = 0;
-    let it = 0;
-    while (s[it++] == '0') {
-        count++;
-    }
-    s = s.substring(count);
+    s = removeLeadingZeroes(s);
     if (less) {
         s = '-' + s;
     }
-    if (s.length == 0) return '0';
+    if (s.length === 0) return '0';
     return s;
 }
 
 module.exports = {
-    apsum: apsum,
-    apsub: apsub,
-    apmul: apmul,
+    apsum,
+    apsub,
+    apmul
 };
--- test/test.js
+++ test/test.js
@@ -2,59 +2,65 @@ var assert = require('assert');
 var apa = require('../apa.js');
 
 describe('addition', function() {
-	it('positive + positive', function() {
-		assert.equal(apa.apsum("5628945656", "3465465978"), "9094411634", 'positive + positive failed')
-	});
-	it('positive + negative', function() {
-		assert.equal(apa.apsum("8750283", "-532235099"), "-523484816", 'positive + negative failed')
-	});
-	it('negative + positive', function() {
-		assert.equal(apa.apsum("-384714", "9999"), "-374715", 'negative + positive failed')
-	});
-	it('negative + negative', function() {
-		assert.equal(apa.apsum("-6", "-7"), "-13", 'negative + negative failed')
-	});
-	it('zero + number', function() {
-		assert.equal(apa.apsum("585", "0"), "585", 'zero + number failed')
-	});
-	it('NaN', function() {
-		assert.throws( function() { apa.apsum("asd", "123"); }, Error, 'doesn\'t throw NaN on invalid input')
-	});
+    it('positive + positive', function() {
+        assert.equal(apa.apsum("5628945656", "3465465978"), "9094411634", 'positive + positive failed')
+    });
+    it('positive + negative', function() {
+        assert.equal(apa.apsum("8750283", "-532235099"), "-523484816", 'positive + negative failed')
+    });
+    it('negative + positive', function() {
+        assert.equal(apa.apsum("-384714", "9999"), "-374715", 'negative + positive failed')
+    });
+    it('negative + negative', function() {
+        assert.equal(apa.apsum("-6", "-7"), "-13", 'negative + negative failed')
+    });
+    it('zero + number', function() {
+        assert.equal(apa.apsum("585", "0"), "585", 'zero + number failed')
+    });
+    it('NaN', function() {
+        assert.throws(function() {
+            apa.apsum("asd", "123")
+        }, Error, 'doesn\'t throw NaN on invalid input')
+    });
 })
 
 describe('subtraction', function() {
-	it('positive - positive', function() {
-		assert.equal(apa.apsub("5628945656", "3465465978"), "2163479678", 'positive - positive failed')
-	});
-	it('positive - negative', function() {
-		assert.equal(apa.apsub("8750283", "-532235099"), "540985382", 'positive - negative failed')
-	});
-	it('negative - positive', function() {
-		assert.equal(apa.apsub("-384714", "9999"), "-394713", 'negative - positive failed')
-	});
-	it('negative - negative', function() {
-		assert.equal(apa.apsub("-6", "-7"), "1", 'negative - negative failed')
-	});
-	it('zero - number', function() {
-		assert.equal(apa.apsub("0", "400"), "-400", 'zero - number failed')
-	});
-	it('NaN', function() {
-		assert.throws( function() { apa.apsum("//", "123"); }, Error, 'doesn\'t throw NaN on invalid input')
-	});
+    it('positive - positive', function() {
+        assert.equal(apa.apsub("5628945656", "3465465978"), "2163479678", 'positive - positive failed')
+    });
+    it('positive - negative', function() {
+        assert.equal(apa.apsub("8750283", "-532235099"), "540985382", 'positive - negative failed')
+    });
+    it('negative - positive', function() {
+        assert.equal(apa.apsub("-384714", "9999"), "-394713", 'negative - positive failed')
+    });
+    it('negative - negative', function() {
+        assert.equal(apa.apsub("-6", "-7"), "1", 'negative - negative failed')
+    });
+    it('zero - number', function() {
+        assert.equal(apa.apsub("0", "400"), "-400", 'zero - number failed')
+    });
+    it('NaN', function() {
+        assert.throws(function() {
+            apa.apsum("//", "123");
+        }, Error, 'doesn\'t throw NaN on invalid input')
+    });
 })
 
 describe('multiplication', function() {
-	it('positive * positive', function() {
-		assert.equal(apa.apmul("5125", "3465"), "17758125", 'positive * positive failed')
-	});
-	it('positive * negative', function() {
-		assert.equal(apa.apmul("123", "-321"), "-39483", 'positive * negative failed')
-	});
-	it('zero * number', function() {
-		assert.equal(apa.apmul("0", "1"), "0", 'zero * number failed')
-	});
-	it('NaN', function() {
-		assert.throws( function() { apa.apsum("", ""); }, Error, 'doesn\'t throw NaN on invalid input')
-	});
+    it('positive * positive', function() {
+        assert.equal(apa.apmul("5125", "3465"), "17758125", 'positive * positive failed')
+    });
+    it('positive * negative', function() {
+        assert.equal(apa.apmul("123", "-321"), "-39483", 'positive * negative failed')
+    });
+    it('zero * number', function() {
+        assert.equal(apa.apmul("0", "1"), "0", 'zero * number failed')
+    });
+    it('NaN', function() {
+        assert.throws(function() {
+            apa.apsum("", "");
+        }, Error, 'doesn\'t throw NaN on invalid input')
+    });
 })
 
